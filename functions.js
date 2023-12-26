0d2d1f57351d4a889f87c90e7bbeacfc9ee44d38
kikopashka <107810372+kikopashka@users.noreply.github.com>
2023-12-26 14:52:42 +0100

    Add files via upload

--- functions.js
+++ functions.js
@@ -83,7 +83,7 @@ export async function jediswapSwap(key, tokenIn, tokenOut, procent){
         } while(general.gwei < gwei)
     }
         
-        const provider = new RpcProvider({ nodeUrl: "https://json-rpc.starknet-mainnet.public.lavanet.xyz" });
+        const provider = new RpcProvider({ nodeUrl: general.providerSTARK });
         const accountAddress = await getArgentAddress(key);
         const account = new Account(provider, accountAddress, key, "1");
 
@@ -143,7 +143,7 @@ export async function myswapSwap(key, tokenIn, tokenOut, procent){
         } while(general.gwei < gwei)
     }
 
-    const provider = new RpcProvider({ nodeUrl: "https://json-rpc.starknet-mainnet.public.lavanet.xyz" });
+    const provider = new RpcProvider({ nodeUrl: providerSTARK });
     const accountAddress = await getArgentAddress(key);
     const account = new Account(provider, accountAddress, key, "1");
 
@@ -208,7 +208,7 @@ export async function kswapSwap(key, tokenIn, tokenOut, procent){
             await delay(15000);
     } while(general.gwei < gwei)
 }
-     const provider = new RpcProvider({ nodeUrl: "https://json-rpc.starknet-mainnet.public.lavanet.xyz" });
+     const provider = new RpcProvider({ nodeUrl: providerSTARK });
      const accountAddress = await getArgentAddress(key);
      const account = new Account(provider, accountAddress, key, "1");
 
@@ -267,7 +267,7 @@ export async function avnuSwap(key, tokenIn, tokenOut, procent){
             await delay(15000);
     } while(general.gwei < gwei)
 }
-    const provider = new RpcProvider({ nodeUrl: "https://json-rpc.starknet-mainnet.public.lavanet.xyz" });
+    const provider = new RpcProvider({ nodeUrl: providerSTARK });
     const accountAddress = await getArgentAddress(key);
     const account = new Account(provider, accountAddress, key, "1");
 
@@ -366,7 +366,7 @@ export async function orbiterBridge(evmKey, starkKey, fromNetwork, procent){
 
 export async function argentWalletGenerate(){
 
-    const provider = new RpcProvider({ nodeUrl: "https://json-rpc.starknet-mainnet.public.lavanet.xyz" });
+    const provider = new RpcProvider({ nodeUrl: providerSTARK });
     //new Argent X account v0.2.3
     const argentXproxyClassHash = "0x25ec026985a3bf9d0cc1fe17326b245dfdc3ff89b8fde106542a3ea56c5a918";
     const argentXaccountClassHash = "0x033434ad846cdd5f23eb73ff09fe6fddd568284a0fb7d1be20ee482f044dabe2";
@@ -406,7 +406,7 @@ export async function argentDeploy(key){
 
 try{
     const accountAddress = await getArgentAddress(key); 
-    const provider = new RpcProvider({ nodeUrl: "https://json-rpc.starknet-mainnet.public.lavanet.xyz" });
+    const provider = new RpcProvider({ nodeUrl: providerSTARK });
     const account = new Account(provider, accountAddress, key, "1");
     const starkKeyPubAX = ec.starkCurve.getStarkKey(key);
     const accountAXsierra = json.parse(fs.readFileSync("./ArgentXaccount030.sierra.json").toString("ascii"));
@@ -447,7 +447,7 @@ export async function dmail(key){
             await delay(15000);
     } while(general.gwei < gwei)
 }
-    const provider = new RpcProvider({ nodeUrl: "https://json-rpc.starknet-mainnet.public.lavanet.xyz" });
+    const provider = new RpcProvider({ nodeUrl: providerSTARK });
     const accountAddress = await getArgentAddress(key);
     const account = new Account(provider, accountAddress, key, "1");
 
@@ -502,7 +502,7 @@ export async function jediswapLP(key, procentMin, procentMax){
             await delay(15000);
     } while(general.gwei < gwei)
 }
-    const provider = new RpcProvider({ nodeUrl: "https://json-rpc.starknet-mainnet.public.lavanet.xyz" });
+    const provider = new RpcProvider({ nodeUrl: providerSTARK });
     const accountAddress = await getArgentAddress(key);
     const account = new Account(provider, accountAddress, key, "1");
    
@@ -576,7 +576,7 @@ export async function jediswapLPWithdrawAll(key){
     } while(general.gwei < gwei)
     }
 
-    const provider = new RpcProvider({ nodeUrl: "https://json-rpc.starknet-mainnet.public.lavanet.xyz" });
+    const provider = new RpcProvider({ nodeUrl: providerSTARK });
     const accountAddress = await getArgentAddress(key);
     const account = new Account(provider, accountAddress, key, "1");
     
@@ -645,7 +645,7 @@ export async function zkLend(key, tokenDeposit, procent, borrow){
     } while(general.gwei < gwei)
 }
 
-    const provider = new RpcProvider({ nodeUrl: "https://json-rpc.starknet-mainnet.public.lavanet.xyz" });
+    const provider = new RpcProvider({ nodeUrl: providerSTARK });
     const accountAddress = await getArgentAddress(key);
     const account = new Account(provider, accountAddress, key, "1");
     
@@ -685,7 +685,7 @@ export async function zkLend(key, tokenDeposit, procent, borrow){
 export async function zkLendBorrow(key, tokenDeposit){
     try{
 
-    const provider = new RpcProvider({ nodeUrl: "https://json-rpc.starknet-mainnet.public.lavanet.xyz" });
+    const provider = new RpcProvider({ nodeUrl: providerSTARK });
     const accountAddress = await getArgentAddress(key);
     const account = new Account(provider, accountAddress, key, "1");
     
@@ -720,7 +720,7 @@ export async function zkLendBorrow(key, tokenDeposit){
 }
 export async function zkLendRepayAll(key, tokenDeposit){
     try{
-    const provider = new RpcProvider({ nodeUrl: "https://json-rpc.starknet-mainnet.public.lavanet.xyz" });
+    const provider = new RpcProvider({ nodeUrl: providerSTARK });
     const accountAddress = await getArgentAddress(key);
     const account = new Account(provider, accountAddress, key, "1");
     
@@ -771,7 +771,7 @@ export async function starkverseMint(key){
     } while(general.gwei < gwei)
 }
     console.log(`Starting minting NFTs on starkverse`);
-    const provider = new RpcProvider({ nodeUrl: "https://json-rpc.starknet-mainnet.public.lavanet.xyz" });
+    const provider = new RpcProvider({ nodeUrl: providerSTARK });
     const accountAddress = await getArgentAddress(key);
     const account = new Account(provider, accountAddress, key, "1");
 
@@ -789,7 +789,7 @@ export async function starkverseMint(key){
 
 export async function swapAllBalanceToToken(key){
     try{
-    const provider = new RpcProvider({ nodeUrl: "https://json-rpc.starknet-mainnet.public.lavanet.xyz" });
+    const provider = new RpcProvider({ nodeUrl: providerSTARK });
     const accountAddress = await getArgentAddress(key);
     const account = new Account(provider, accountAddress, key, "1");
 
@@ -869,7 +869,7 @@ export async function randomswap(key, tokenIn, tokenOut, procent){
                 await delay(15000);
         } while(general.gwei < gwei)
     }
-        const provider = new RpcProvider({ nodeUrl: "https://json-rpc.starknet-mainnet.public.lavanet.xyz" });
+        const provider = new RpcProvider({ nodeUrl: providerSTARK });
         const accountAddress = await getArgentAddress(key);
         const account = new Account(provider, accountAddress, key, "1");
         console.log(`Minting starknet identity`)
@@ -887,7 +887,7 @@ export async function randomswap(key, tokenIn, tokenOut, procent){
 
   export async function checkAllBalance(key){
     const accountAddress = await getArgentAddress(key);
-    const provider = new RpcProvider({ nodeUrl: "https://json-rpc.starknet-mainnet.public.lavanet.xyz" });
+    const provider = new RpcProvider({ nodeUrl: providerSTARK });
     const contractETH = new Contract(abi.erc20token, config.tokens.ETH, provider);
     const balanceETH = await contractETH.balanceOf(accountAddress);
 
--- settings.js
+++ settings.js
@@ -6,10 +6,11 @@ export class general{
     static delayAfterTxMax = 70
     static delayAfterProjectMin = 60
     static delayAfterProjectMax = 170
+    static providerSTARK = "https://starknet-mainnet.infura.io/v3/8e78eff4b1de4f0680121d689574bcfc"
     static provider = "https://eth.llamarpc.com"
     static providerARB = "https://arbitrum.llamarpc.com"
     static gweiL1 = 20                               //gwei L1 при котром будут работать функции связанные с EVM сетями
-    static gwei = 23                                 //gwei старка при котором будут работать функции старка
+    static gwei = 27                                 //gwei старка при котором будут работать функции старка
 }
 
 export class checkBalance{
@@ -51,7 +52,7 @@ export class jediswap{
     static mode = false                     //Если true, то функция будет работать, если false, то не будет 
     static tokenIn = "ETH"                 //Какой токен бриджим свапаем в первом(!) свапе
     static swap_number_min = 1
-    static swap_number_max = 3           //Количество свапов на этом DEX
+    static swap_number_max = 1           //Количество свапов на этом DEX
     static procent_first_swap_min = 60     //Минимальный % для первого свапа ETH
     static procent_first_swap_max = 80     //Максимальный % для первого свапа ЕТН
 }
@@ -67,7 +68,7 @@ export class myswap{
 }
 
 export class kswap{
-    static mode = false
+    static mode = true
     static tokenIn = "ETH"
     static swap_number_min = 1
     static swap_number_max = 1
@@ -79,7 +80,7 @@ export class avnu{
     static mode = false
     static tokenIn = "ETH"
     static swap_number_min = 1
-    static swap_number_max = 3
+    static swap_number_max = 1
     static procent_first_swap_min = 60
     static procent_first_swap_max = 70
 }
@@ -93,7 +94,7 @@ export class starkkVerse{
 export class dmailClass{
     static mode = false
     static emailToSendMin = 1          //минимальное количество писем
-    static emailToSendMax = 3           //максимальное количество писем
+    static emailToSendMax = 2          //максимальное количество писем
    // static delayMin = 10                 //минимальная задержка 
    // static delayMax = 100                //максимальная задержка
 }
@@ -105,7 +106,7 @@ export class jediLP{
 }
 
 export class zkLendClass{
-    static mode = true                      
+    static mode = false                      
     static borrow = false                   //Если true, то будет ещё занимать/выплачивать
     static procentMin = 15                  //минимальный процент от баланса токена для депозита
     static procentMax = 45                  //максимальный процент от баланса токена для депозита
